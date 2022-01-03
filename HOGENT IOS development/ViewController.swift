3d1184c7f704edca1d54a694f793fe0f7c569172
Laurens Van Bruwaene <laurens.vanbruwaene@gmail.com>
2022-01-03 22:19:07 +0100

    Initial Commit

---
+++ HOGENT IOS development/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HOGENT IOS development
+//
+//  Created by LaurensVB on 03/01/2022.
+//  Copyright © 2022 LaurensVB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
